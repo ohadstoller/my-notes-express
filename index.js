c5ee67be58c0cf432f3a0658f9b2e31a7b56190e
ohadstoller <ohadstoller@gmail.com>
2021-06-17 18:53:02 +0300

    error handling for non existed note id

--- index.js
+++ index.js
@@ -53,10 +53,25 @@ app.get('/api/notes', (request, response) => {
 })
 
 
+// app.get('/api/notes/:id', (request, response) => {
+//   Note.findById(request.params.id).then(note => {
+//     response.json(note)
+//   })
+// })
+
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.post('/api/notes', (request, response) => {
@@ -77,11 +92,6 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({error: 'unknown endpoint'})
