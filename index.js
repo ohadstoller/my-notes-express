5d1ca6504a9b7867fe9770be497ec459b27ccf94
ohadstoller <ohadstoller@gmail.com>
2021-06-19 16:49:17 +0300

    adding async await to get and post routes

--- index.js
+++ index.js
@@ -25,41 +25,47 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+app.get('/api/notes', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', async (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
     return response.status(400).json({error: 'content missing'})
   }
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  })
+  try {
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+    })
 
-  note.save().then(savedNote => {
+    const savedNote = await note.save()
     response.json(savedNote)
-  })
+
+  } catch (e) {
+    next(e)
+  }
+
+
 })
 
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+app.get('/api/notes/:id', async (request, response, next) => {
+  try {
+    const requestedNote = await Note.findById(request.params.id)
+    if (requestedNote) {
+      response.json(requestedNote)
+    } else {
+      response.status(404).end()
+    }
+  } catch (e) {
+    next(e)
+  }
 })
 
 app.delete('/api/notes/:id', async (request, response, next) => {
